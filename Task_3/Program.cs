9d2002418fdc4d6e8d9b34ade66c2d0db08722ed
Barbara <varya.sheptunova@gmail.com>
2022-12-22 19:35:43 +0300

    Task 3 Even numbers

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+WriteLine("Является ли число четным?");
+
+if (number % 2 == 0)
+{
+    WriteLine("Да, является");
+}
+else
+{
+    WriteLine("Нет, не является");
+}
