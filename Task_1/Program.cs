c82953b5302b5cf0d2214cede377d3be1453926c
Barbara <varya.sheptunova@gmail.com>
2022-12-22 19:24:15 +0300

    Task 1 Choosing min and max

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите число 1: ");
+int number_1 = int.Parse(ReadLine()!);
+WriteLine("Введите число 2: ");
+int number_2 = int.Parse(ReadLine()!);
+
+if (number_1 > number_2)
+{
+    Write("Наименьшее число: ");
+    WriteLine(number_2);
+    Write("Наибольшее число: ");
+    WriteLine(number_1);
+}
+else
+{
+    Write("Наименьшее число: ");
+    WriteLine(number_1);
+    Write("Наибольшее число: ");
+    WriteLine(number_2);
+}
