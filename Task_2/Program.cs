446c889cf397826c123ae72305afa4750974c2da
Barbara <varya.sheptunova@gmail.com>
2022-12-22 19:31:30 +0300

    Task 2 Choosing max among 3 numbers

---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+WriteLine("Введите число 1: ");
+int number_1 = int.Parse(ReadLine()!);
+WriteLine("Введите число 2: ");
+int number_2 = int.Parse(ReadLine()!);
+WriteLine("Введите число 3: ");
+int number_3 = int.Parse(ReadLine()!);
+
+int max = number_1;
+
+if (number_1 > max)
+{
+    max = number_1;
+}
+if (number_2 > max)
+{
+    max = number_2;
+}
+if (number_3 > max)
+{
+    max = number_3;
+}
+
+Write("Максимальное число: ");
+WriteLine(max);
