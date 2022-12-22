619a780535515307d2cfb104a8bee35aa3a0e6b3
Barbara <varya.sheptunova@gmail.com>
2022-12-22 19:49:02 +0300

    Task 4 Printing even numbers in range

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int number_1 = int.Parse(ReadLine()!);
+int n = number_1 - 1;
+while (n > 1)
+{
+    if (n % 2 == 0)
+    {
+        Write($"{n} ");
+    }
+    n--;
+}
